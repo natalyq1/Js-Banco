dc17cac96df0de58a67e13bbb2b5a397f44b016d
natalyq1 <onatylili@gmail.com>
2023-03-08 11:39:13 -0500

    two classes with dat

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 class Cliente{
     nombreCliente;
     dniCliente;
-    numeroCuenta;
-    saldoCuenta;
+    rutCliente;
 }
+
+class CuentaCorriente{
+    numero;
+    saldo;
+    agencia;
+}
+
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
 
 cliente1.nombreCliente ="Jose"
 cliente1.dniCliente = "13232"
-cliente1.numeroCuenta ="5121215"
-cliente1.saldoCuenta = 1200000
+cliente1.rutCliente = "13232"
+
+const cuentaCorriente1 = new CuentaCorriente();
+
+cuentaCorriente1.numero ="5121215"
+cuentaCorriente1.saldo = 1200000
+cuentaCorriente1.agencia ="Independiente"
+
+const cliente2 = new Cliente();
 
 cliente2.nombreCliente ="Marco"
 cliente2.dniCliente = "32555"
-cliente2.numeroCuenta ="5125698"
-cliente2.saldoCuenta = 512000023
+cliente2.rutCliente = "32555"
+
+const cuentaCorriente2 = new CuentaCorriente();
+
+cuentaCorriente2.numero ="5125698"
+cuentaCorriente2.saldo = 512000023
+cuentaCorriente2.agencia ="Colegio"
 
 console.log(cliente1);
+console.log(cuentaCorriente1);
 console.log(cliente2);
+console.log(cuentaCorriente2);
