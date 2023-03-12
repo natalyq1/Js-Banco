b03e37fa01e90c38f3db0658ba5c6b4ac4ca94ed
natalyq1 <onatylili@gmail.com>
2023-03-12 13:26:18 -0500

    a class Cuenta was done to generalize the functions of an account

---
+++ Cuenta.js
@@ -0,0 +1,36 @@
+
+export class Cuenta {
+    #cliente
+    #saldo
+
+    constructor(cliente, numero, agencia, saldo){
+        this.numero = numero
+        this.agencia = agencia
+        this.#cliente = cliente
+        this.#saldo = saldo
+    }
+
+    //metodo
+    depositoEnCuenta(valor){
+        //el this se usa para referirnos a esta cuenta, en la que se este actualmente
+        if (valor > 0) 
+           this.#saldo += valor
+        return this.#saldo
+    }
+
+    retirarDeCuenta(valor){
+        if (valor <= this.#saldo) 
+            this.#saldo -= valor
+        return this.#saldo
+    }
+
+    verSaldo(){
+        return this.#saldo
+    }
+
+    transferirParaCuenta(valor, cuentaDestino){
+        this.retirarDeCuenta(valor)
+        cuentaDestino.depositoEnCuenta(valor)
+    }
+}
+
--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 
 //importacion de clases
 import { Cliente } from "./Cliente.js"
-import { CuentaCorriente } from "./CuentaCorriente.js";
-import { CuentaAhorro } from "./CuentaAhorro.js"
+//import { CuentaCorriente } from "./CuentaCorriente.js";
+//import { CuentaAhorro } from "./CuentaAhorro.js"
+import { Cuenta } from "./Cuenta.js"
+
 
 //esta debe definirse como una variable para funcionar por modulos
 const cliente = new Cliente('Raul', '133548121', '1255548')//ahora el cliente espera ciertos parametros luego de haber definido el constructor 
 const cliente2 = new Cliente('Maria', '233548121', '33355548')
 
-const cuentaDeRaul = new CuentaCorriente(cliente, '1', '001' )
-const cuentaDeMaria = new CuentaCorriente(cliente2, '2', '002' )
+const cuentaDeRaul = new Cuenta(cliente, '1', '001', 0 )
+const cuentaDeMaria = new Cuenta(cliente2, '2', '002', 0 )
 
 console.log(cliente);
 console.log(cliente2);
 
-const cuentaAhorroRaul = new CuentaAhorro(cliente, '9985','001', 0)
- console.log(cuentaDeRaul);
- console.log(cuentaAhorroRaul);
+const cuentaAhorroRaul = new Cuenta(cliente, '9985','001', 0)
+console.log(cuentaDeRaul);
+cuentaDeRaul.depositoEnCuenta(150)
+console.log(cuentaDeRaul.verSaldo());
+cuentaDeRaul.retirarDeCuenta(30)
+console.log(cuentaDeRaul.verSaldo());
+
+console.log(cuentaAhorroRaul);
+cuentaAhorroRaul.depositoEnCuenta(120)
+console.log(cuentaAhorroRaul.verSaldo());
+cuentaAhorroRaul.retirarDeCuenta(30)
+console.log(cuentaAhorroRaul.verSaldo());
  /* 
-CuentaCorriente { numero: '1', agencia: '001' } 
-CuentaAhorro { numero: '9985', agencia: '001' }
+Cuenta { numero: '1', agencia: '001' }
+150
+120
+Cuenta { numero: '9985', agencia: '001' }       
+120
+90
  */
