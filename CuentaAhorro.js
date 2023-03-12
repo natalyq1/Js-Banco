5ec81e716ae9e65b57f6065ef98b26396b99248e
natalyq1 <onatylili@gmail.com>
2023-03-12 13:13:55 -0500

    CuentaAhorro created

---
+++ CuentaAhorro.js
@@ -0,0 +1,34 @@
+export class CuentaAhorro{
+    #cliente
+    #saldo
+
+    constructor(cliente, numero, agencia, saldo){
+        this.numero = numero
+        this.agencia = agencia
+        this.#cliente = cliente
+        this.#saldo = saldo
+    }
+
+    //metodo
+    depositoEnCuenta(valor){
+        //el this se usa para referirnos a esta cuenta, en la que se este actualmente
+        if (valor > 0) 
+           this.#saldo += valor
+        return this.#saldo
+    }
+
+    retirarDeCuenta(valor){
+        if (valor <= this.#saldo) 
+            this.#saldo -= valor
+        return this.#saldo
+    }
+
+    verSaldo(){
+        return this.#saldo
+    }
+
+    transferirParaCuenta(valor, cuentaDestino){
+        this.retirarDeCuenta(valor)
+        cuentaDestino.depositoEnCuenta(valor)
+    }
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 //importacion de clases
 import { Cliente } from "./Cliente.js"
 import { CuentaCorriente } from "./CuentaCorriente.js";
+import { CuentaAhorro } from "./CuentaAhorro.js"
 
 //esta debe definirse como una variable para funcionar por modulos
 const cliente = new Cliente('Raul', '133548121', '1255548')//ahora el cliente espera ciertos parametros luego de haber definido el constructor 
@@ -13,32 +14,10 @@ const cuentaDeMaria = new CuentaCorriente(cliente2, '2', '002' )
 console.log(cliente);
 console.log(cliente2);
 
-console.log(CuentaCorriente.cantidadCuentas); //2
-//para ver saldo debo crear una variable que va a variar a medida de la ejecucion del programa
-let saldo = cuentaDeRaul.verSaldo()
-
-
-cuentaDeRaul.depositoEnCuenta(100)
-
-cuentaDeRaul.retirarDeCuenta(30)
-
-cuentaDeRaul.depositoEnCuenta(220)
-saldo = cuentaDeRaul.verSaldo()
-console.log(`El saldo actual de la cuenta de Raul es ${saldo}`);
-
-cuentaDeRaul.transferirParaCuenta(50,cuentaDeMaria);
-saldo = cuentaDeRaul.verSaldo()
-console.log(`El saldo actual de la cuenta de Raul es ${saldo} despues de haber transferido a Maria`);
-/*
-Cliente {
-  nombreCliente: 'Raul',
-  dniCliente: '133548121',
-  rutCliente: '1255548'
-}
-Cliente {
-  nombreCliente: 'Maria',
-  dniCliente: '233548121',
-  rutCliente: '33355548'
-}*/ 
-
-
+const cuentaAhorroRaul = new CuentaAhorro(cliente, '9985','001', 0)
+ console.log(cuentaDeRaul);
+ console.log(cuentaAhorroRaul);
+ /* 
+CuentaCorriente { numero: '1', agencia: '001' } 
+CuentaAhorro { numero: '9985', agencia: '001' }
+ */
